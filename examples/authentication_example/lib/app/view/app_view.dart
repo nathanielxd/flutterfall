462f2c6bb318f289d95abd47e99e0423a8749ccc
Nathaniel Dragusin <dragusinnathaniel@gmail.com>
2021-10-21 17:05:17 +0100

    update

---
+++ examples/authentication_example/lib/app/view/app_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:authentication/app/app.dart';
+
+class AppView extends StatelessWidget {
+
+  AppView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'flutterfall',
+      home: Scaffold()
+    );
+  }
+}
